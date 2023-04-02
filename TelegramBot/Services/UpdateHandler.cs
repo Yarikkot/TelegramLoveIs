11b00de089d0402e89fa769efefdcc396f22767c
Yaroslav Kovalenko <yaroslav.kovalenko@eur.cushwake.com>
2023-04-02 19:05:49 +0300

    Правки по работе с админом

--- TelegramBot/Services/UpdateHandler.cs
+++ TelegramBot/Services/UpdateHandler.cs
@@ -15,6 +15,7 @@ namespace Telegram.Bot.Services
         private readonly IComplimentService complimentService;
 
         private const string InvalidText = "Нельзя устанавливать пустые строки и строки которые начинаются с \'/\'";
+        private const string adminInfoPath = "admin";
 
         private string buttonText = "Комплиментик для красотки";
         private string usageText = "Тебе нужно нажать кнопочку которая есть на клавиатуре и будет счастье :)";
@@ -25,6 +26,7 @@ namespace Telegram.Bot.Services
         private string addCommand = "/add";
         private string showCommand = "/show";
         private string adminCommand = "/admin";
+        private string clearAdminCommand = "/clearAdmin";
 
         private TimeSpan complimentCooldown = TimeSpan.FromHours(1);
         private DateTime lastCompliment = DateTime.MinValue;
@@ -39,6 +41,13 @@ namespace Telegram.Bot.Services
 
         public UpdateHandler(ITelegramBotClient botClient, IComplimentService complimentService)
         {
+            if (System.IO.File.Exists(adminInfoPath))
+            {
+                if (long.TryParse(System.IO.File.ReadAllText(adminInfoPath), out var id))
+                {
+                    adminId = id;
+                }
+            }
             this._botClient = botClient ?? throw new ArgumentNullException(nameof(botClient));
             this.complimentService = complimentService ?? throw new ArgumentNullException(nameof(complimentService));
         }
@@ -67,6 +76,10 @@ namespace Telegram.Bot.Services
             {
                 await Admin(message, cancellationToken);
             }
+            else if (message.Text == clearAdminCommand)
+            {
+                await ClearAdmin(message, cancellationToken);
+            }
             else if (message.Text.StartsWith(addCommand))
             {
                 await AddCompliment(message, cancellationToken);
@@ -94,39 +107,13 @@ namespace Telegram.Bot.Services
             await SendMessage(usageText, message.Chat.Id, cancellationToken);
         }
 
-        private async Task AddCompliment(Message message, CancellationToken cancellationToken)
-        {
-            var compliment = CleanFromCommand(message.Text!, addCommand);
-            if (ValidateText(compliment))
-            {
-                complimentService.AddCompliment(compliment);
-                await SendMessage($"\"{compliment}\" успешно добавлен, теперь их {complimentService.GetComplimentCount()} в запасе!", message.Chat.Id, cancellationToken);
-            }
-            else
-            {
-                await SendMessage(InvalidText, message.Chat.Id, cancellationToken);
-            }
-        }
-
-        private async Task ShowCompliments(Message message, CancellationToken cancellationToken)
-        {
-            var compliments = complimentService.GetAllCompliments();
-            await SendMessage($"Вот все комплименты которые остались:\n{string.Join("\n", compliments)}", message.Chat.Id, cancellationToken);
-        }
-
         private async Task MakeСompliment(Message message, CancellationToken cancellationToken)
         {
-            if (complimentService.GetComplimentCount() == 0)
-            {
-                await SendMessage(noComplimentText, message.Chat.Id, cancellationToken);
-                return;
-            }
-
             string compliment;
             var timePassed = DateTime.Now - lastCompliment;
             if (DateTime.Now - lastCompliment > complimentCooldown)
             {
-                compliment = complimentService.GetCompliment();
+                compliment = complimentService.GetCompliment() ?? noComplimentText;
                 if (adminId != null)
                 {
                     var count = complimentService.GetComplimentCount();
@@ -151,44 +138,98 @@ namespace Telegram.Bot.Services
             lastCompliment = DateTime.Now;
         }
 
+        private async Task AddCompliment(Message message, CancellationToken cancellationToken)
+        {
+            if (Auth(message.Chat.Id))
+            {
+                var compliment = CleanTextFromCommand(message.Text!, addCommand);
+                if (ValidateText(compliment))
+                {
+                    complimentService.AddCompliment(compliment);
+                    await SendMessage($"\"{compliment}\" успешно добавлен, теперь их {complimentService.GetComplimentCount()} в запасе!", message.Chat.Id, cancellationToken);
+                }
+                else
+                {
+                    await SendMessage(InvalidText, message.Chat.Id, cancellationToken);
+                }
+            }
+        }
+
+        private async Task ShowCompliments(Message message, CancellationToken cancellationToken)
+        {
+            if (Auth(message.Chat.Id))
+            {
+                var compliments = complimentService.GetAllCompliments();
+                await SendMessage($"Вот все комплименты которые остались:\n{string.Join("\n", compliments)}", message.Chat.Id, cancellationToken);
+            }
+        }
+
         private async Task Admin(Message message, CancellationToken cancellationToken)
         {
-            adminId = message.Chat.Id;
-            var text = "Доступные команды для админа:\n" +
-                $"{changeButtonTextCommand} новый текст - изменить текст кнопки (сейчас {buttonText})\n" +
-                $"{changeUsageTextCommand} новый текст - изменить текст сообщения (сейчас {usageText})\n" +
-                $"{addCommand} новый комплимент - добавить новый комплимент (сейчас осталось комплиментов: {complimentService.GetComplimentCount()} )\n" +
-                $"{showCommand} - посмотреть все оставшиеся комплименты\n" +
-                $"Так же при выполнении команды {adminCommand} - ваш ID запомнился для оповещения об оставшихся комплиментах.";
-            await SendMessage(text, message.Chat.Id, cancellationToken);
+            if (adminId is null)
+            {
+                adminId = message.Chat.Id;
+                System.IO.File.WriteAllText(adminInfoPath, adminId.ToString());
+            }
+
+            if (Auth(message.Chat.Id))
+            {
+                var text = "Доступные команды для админа:\n" +
+                    $"{changeButtonTextCommand} новый текст - изменить текст кнопки (сейчас {buttonText})\n" +
+                    $"{changeUsageTextCommand} новый текст - изменить текст сообщения (сейчас {usageText})\n" +
+                    $"{addCommand} новый комплимент - добавить новый комплимент (сейчас осталось комплиментов: {complimentService.GetComplimentCount()} )\n" +
+                    $"{showCommand} - посмотреть все оставшиеся комплименты\n" +
+                    $"Так же при выполнении команды {adminCommand} - ваш ID запомнился для оповещения об оставшихся комплиментах.";
+                await SendMessage(text, message.Chat.Id, cancellationToken);
+            }
+        }
+
+        private async Task ClearAdmin(Message message, CancellationToken cancellationToken)
+        {
+            if (Auth(message.Chat.Id))
+            {
+                if (System.IO.File.Exists(adminInfoPath))
+                {
+                    System.IO.File.Delete(adminInfoPath);
+                }
+
+                adminId = null;
+                await SendMessage($"Админ успешно удалён! Можете заново его переназначит командой {adminCommand}", message.Chat.Id, cancellationToken);
+            }
         }
 
         private async Task ChangeButtonText(Message message, CancellationToken cancellationToken)
         {
-            var newText = CleanFromCommand(message.Text!, changeButtonTextCommand);
+            if (Auth(message.Chat.Id))
+            {
+                var newText = CleanTextFromCommand(message.Text!, changeButtonTextCommand);
 
-            if (ValidateText(newText))
-            {
-                buttonText = newText;
-                await SendMessage($"Новый текст для кнопки успешно установлен на {newText}", message.Chat.Id, cancellationToken);
-            }
-            else
-            {
-                await SendMessage($"Нельзя устанавливать пустые строки и строки которые начинаются с \'/\'", message.Chat.Id, cancellationToken);
+                if (ValidateText(newText))
+                {
+                    buttonText = newText;
+                    await SendMessage($"Новый текст для кнопки успешно установлен на {newText}", message.Chat.Id, cancellationToken);
+                }
+                else
+                {
+                    await SendMessage($"Нельзя устанавливать пустые строки и строки которые начинаются с \'/\'", message.Chat.Id, cancellationToken);
+                }
             }
         }
 
         private async Task ChangeUsageText(Message message, CancellationToken cancellationToken)
         {
-            var newText = CleanFromCommand(message.Text!, changeUsageTextCommand);
-            if (ValidateText(newText))
+            if (Auth(message.Chat.Id))
             {
-                usageText = newText;
-                await SendMessage($"Новый текст сообщения успешно установлен на {newText}", message.Chat.Id, cancellationToken);
-            }
-            else
-            {
-                await SendMessage(InvalidText, message.Chat.Id, cancellationToken);
+                var newText = CleanTextFromCommand(message.Text!, changeUsageTextCommand);
+                if (ValidateText(newText))
+                {
+                    usageText = newText;
+                    await SendMessage($"Новый текст сообщения успешно установлен на {newText}", message.Chat.Id, cancellationToken);
+                }
+                else
+                {
+                    await SendMessage(InvalidText, message.Chat.Id, cancellationToken);
+                }
             }
         }
 
@@ -197,7 +238,7 @@ namespace Telegram.Bot.Services
             return !string.IsNullOrEmpty(text) && !text.StartsWith('/');
         }
 
-        private string CleanFromCommand(string message, string command)
+        private string CleanTextFromCommand(string message, string command)
         {
             return message.Replace(command, "").Trim();
         }
@@ -231,6 +272,14 @@ namespace Telegram.Bot.Services
             return Task.CompletedTask;
         }
 
+        private bool Auth(long id)
+        {
+            if (adminId == null)
+                return false;
+
+            return id == adminId;
+        }
+
         public async Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
             var ErrorMessage = exception switch
@@ -239,7 +288,6 @@ namespace Telegram.Bot.Services
                 _ => exception.ToString()
             };
 
-            // Cooldown in case of network connection error
             if (exception is RequestException)
                 await Task.Delay(TimeSpan.FromSeconds(2), cancellationToken);
         }
